46a5e2cbce7eadddac01d0e93383952d72a80aa0
dnjs2721 <skadu66@gmail.com>
2022-09-05 08:59:52 +0900

    Algorithm

--- ConnectList/Algorithm_03.py
+++ ConnectList/Algorithm_03.py
@@ -1,14 +1,13 @@
 import sys
-N, K = map(int, sys.stdin.readline().strip().split())
+N, K = map(int,sys.stdin.readline().strip().split())
 arr = [i for i in range(1, N+1)]
 stack = []
 num = 0
-
-for i in range(N):
+for i in range(N) :
     num += K-1
     if num >= len(arr):
         num = num%len(arr)
-    
+
     stack.append(str(arr.pop(num)))
 
-print("<",",".join(stack),">", sep='')
+print("<",", ".join(stack),">",sep='')
