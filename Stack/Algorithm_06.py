7e7f6d033311da55d2ca7b542091a9b0133bec8d
dnjs2721 <skadu66@gmail.com>
2022-09-15 01:33:12 +0900

    Algorithm

--- Stack/Algorithm_06.py
+++ Stack/Algorithm_06.py
@@ -15,15 +15,13 @@
 import sys
 N = int(sys.stdin.readline().strip())
 arr = list(map(int, sys.stdin.readline().strip().split()))
-stack = [-1]*N
+res = [-1]*N
+stack = []
 
-for i in range(N-1):
-    while i+1 < N:
-        if arr[i] >= arr[i+1]:
-            arr.pop(i)
-        else:
-            stack[i]= arr[i+1]
-            print(arr)
-            print(stack)
-            break
-print(stack)
+stack.append(0)
+for i in range(1,N):
+    while stack and arr[stack[-1]] < arr[i]:
+        res[stack.pop()] = arr[i]
+    stack.append(i)
+
+print(*res)
