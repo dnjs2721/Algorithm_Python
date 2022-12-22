068bb51a8bd40f1c9e8972d4fe49a6a9fa933d75
dnjs2721 <skadu66@gmail.com>
2022-12-22 10:11:02 +0900

    Baek

--- BFS/Baek10026.py
+++ BFS/Baek10026.py
@@ -10,37 +10,36 @@ def bfs(a, b):
     dx = [0, 0, 1, -1]
     dy = [1, -1, 0, 0]
     q.append((a, b))
-    visited[a][b] = 1
+    visted[a][b] = 1
     while q:
         x, y =  q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < n and dist[nx][ny] == dist[x][y] and not visited[nx][ny]:
-                visited[nx][ny] = 1
-                q.append((nx, ny))
+            if 0 <= nx < n and 0 <= ny < n and dist[nx][ny] == dist[x][y] and not visted[nx][ny]:
+                visted[nx][ny] = 1
+                q.append((nx, ny)) 
+
 
 # 적록색약이 아닌 경우
-visited = [[0] * n for _ in range(n)]
+visted = [[0] * n for _ in range(n)]
 cnt1 = 0
 for i in range(n):
     for j in range(n):
-        if not visited[i][j]:
+        if not visted[i][j]:
             bfs(i, j)
             cnt1 += 1
 
 # 적록색약인 경우
-for i in range(n):
-    for j in range(n):
-        if dist[i][j] == 'G':
-            dist[i][j] = 'R'
-print(dist)
-
-visited = [[0] * n for _ in range(n)]
+visted = [[0] * n for _ in range(n)]
 cnt2 = 0
 for i in range(n):
     for j in range(n):
-        if not visited[i][j]:
+        if dist[i][j] == 'R':
+            dist[i][j] = 'G'
+for i in range(n):
+    for j in range(n):
+        if not visted[i][j]:
             bfs(i, j)
             cnt2 += 1
 
