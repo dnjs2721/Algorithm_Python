4bf6057d9bd966ad5ed97f117b47659e1f707744
dnjs2721 <skadu66@gmail.com>
2023-02-28 17:45:42 +0900

    Programmers

--- Programmers/[답지참조]테이블 해시 함수.py
+++ Programmers/[답지참조]테이블 해시 함수.py
@@ -1,6 +1,6 @@
 def solution(data, col, row_begin, row_end):
     answer = 0
-    data = sorted(data, key = lambda x : [x[col - 1], x[0]])
+    data = sorted(data, key = lambda x : [x[col - 1], -x[0]])
     
     for i in range(row_begin, row_end + 1):
         total = 0
