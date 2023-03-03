10f91225068bf38b8127e43e13476f11eb1941aa
dnjs2721 <skadu66@gmail.com>
2023-03-03 21:44:50 +0900

    Programmers

--- Programmers/억억단을 외우자.py
+++ Programmers/억억단을 외우자.py
@@ -1,26 +1,19 @@
 def solution(e, starts):
-    answer = []
+    answer = [0 for _ in range(e+1)]
     divisor = [1 for _ in range(e+1)]
     for i in range(2, e+1):
         for j in range(i, e+1, i):
             divisor[j] += 1
-        
-    stores = [0 for _ in range(e+1)]
-    max_count = divisor[e]
-    max_index = e
-    for i in range(e, 0, -1):
-        if i == e:
-            stores[i] = i
-            continue
+            
+    max_count, max_index = divisor[e], e
+    answer[e] = e
+    for i in range(e-1, 0, -1):
         if max_count <= divisor[i]:
             max_count = divisor[i]
             max_index = i
-        stores[i] = max_index
+        answer[i] = max_index
         
-    for s in starts:
-        answer.append(stores[s])
-        
-    return answer
+    return [answer[s] for s in starts]
 
 print(solution(8, [1,3,7]))
 
@@ -29,12 +22,12 @@ print(solution(8, [1,3,7]))
 # 2중 for문을 이용하여 2부터 e까지의 (i)약수를 구한다. 1의 약수는 구하지 않아도 된다.
 # 이때 i에서 e까지 i의 배수의 개수만큼 divisor의 i인덱스에 저장한다.
 
-# 약수를 가장많이 가지고 있는 수의 인덱스를 저장하기 위한 배열 stores를 생성
+# 약수를 가장많이 가지고 있는 수의 인덱스를 저장하기 위한 배열 answer
 # e 에서 1번 인덱스 방향으로 조회할 것 이기에 max_count 와 max_index 는 각각 divisor[e] 와 e로 선언
 # max_count 보다 큰 값이 나오면 max_count 와 max_index 를 변경
-# stores의 해당 인덱스에 max_index 값을 저장
+# answer 해당 인덱스에 max_index 값을 저장
 
-# stores[s] 를 조회하여 답 출력
+# answer[s] 를 조회하여 답 출력
 
 # s의 목록 starts
 # 각 s에 대해 서 임의의 수 n  s <= n <= e
