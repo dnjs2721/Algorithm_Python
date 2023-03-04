7291921898a08bf858649429d2efebd50f211ee7
dnjs2721 <skadu66@gmail.com>
2023-03-04 23:47:14 +0900

    Programmers

--- Programmers/숫자 카드 나누기.py
+++ Programmers/숫자 카드 나누기.py
@@ -1,34 +1,31 @@
-def solution(arrayA, arrayB):
-    def two_gcd(a, b):
+def gcd(a, b):
         while b != 0:
             a, b = b, a % b
         return a
     
-    def check(arr, gcd):
-        for item in arr:
-            if item % gcd == 0:
-                return 0
-        return 1
-    
+def check(arr, gcd):
+    for item in arr:
+        if item % gcd == 0:
+            return 0
+    return 1
+
+def solution(arrayA, arrayB):
     a_gcd = arrayA[0]
     b_gcd = arrayB[0]
     for i in range(len(arrayA)):
-        a_gcd = two_gcd(a_gcd, arrayA[i])
-        b_gcd = two_gcd(b_gcd, arrayB[i])
+        a_gcd = gcd(a_gcd, arrayA[i])
+        b_gcd = gcd(b_gcd, arrayB[i])
     
-    if a_gcd == 1:
-        if check(arrayA, b_gcd) == 0:
-            b_gcd = 0
-            
-    elif b_gcd == 1:
-        if check(arrayB, a_gcd) == 0:
-            a_gcd = 0
+    if check(arrayA, b_gcd) == 0:
+        b_gcd = 1
+    if check(arrayB, a_gcd) == 0:
+        a_gcd = 1
     
-    if a_gcd == 0 or b_gcd == 0:
+    if a_gcd == 1 and b_gcd == 1:
         return 0
-    else:           
+    else:
         return max(a_gcd, b_gcd)
 
 print(solution([10, 17], [5, 20]))
 print(solution([10, 20], [5, 17]))
-print(solution([14, 35, 119], [18, 30, 102]))
+print(solution([14, 35, 42], [18, 30, 102]))
